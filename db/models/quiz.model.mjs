eb57e3d471e96daa5beedca974b9d80ee81594ae
chetu97 <chaitanya.sanoriya@gmail.com>
2021-04-17 17:57:01 -0400

    update for quiz

--- db/models/quiz.model.mjs
+++ db/models/quiz.model.mjs
@@ -1,27 +1,33 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const quizSchema = new mongoose.Schema({
-    name: String,
-    typeOfQuiz: String,
-    questions: 
-        [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Question'
-        }],
-    max_scores: [{
-        // NOTE: top 10/20 scores stored here
-        // if user among them then additional field added at the end
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-        },
-        score: Number,
-    }],
-    description: String,
-    lastPlayed: Date,
-    noOfPlays: Number
-}, {timestamps: true});
+const quizSchema = new mongoose.Schema(
+    {
+        name: String,
+        typeOfQuiz: Number,
+        questions: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Question",
+            },
+        ],
+        max_scores: [
+            {
+                // NOTE: top 10/20 scores stored here
+                // if user among them then additional field added at the end
+                user: {
+                    type: mongoose.Schema.Types.String,
+                    ref: "User",
+                },
+                score: Number,
+            },
+        ],
+        description: String,
+        lastPlayed: Date,
+        noOfPlays: Number,
+    },
+    { timestamps: true }
+);
 
-const Quiz = mongoose.model('Quiz', quizSchema);
+const Quiz = mongoose.model("Quiz", quizSchema);
 
 export default Quiz;
--- routes/user.mjs
+++ routes/user.mjs
@@ -1,33 +1,31 @@
-import express from 'express';
-import models from '../db/db_main.mjs';
+import express from "express";
+import models from "../db/db_main.mjs";
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-    if(!req.user) {
-        res.status(400).json({Message: "Login before fetching details"});
+router.get("/", async (req, res, next) => {
+    console.log("req.header: ", req.headers);
+    if (!req.user) {
+        res.status(400).json({ Message: "Login before fetching details" });
     }
     try {
         const userID = req.user._id;
         const user = await models.User.findById(userID);
         res.json(user);
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err);
-        res.status(500).json({Message: "Error while fetching user"});
-    }
-})
-
-router.get('/all', async (req, res, next) => {
-    try {
-        const users = await models.User.find();
-        res.json(users);
-    }
-    catch(err) {
-        console.log("Error fetching users.\n", err);
-        res.json({Message : "Error fetching users"});
+        res.status(500).json({ Message: "Error while fetching user" });
     }
 });
 
+router.get("/all", async (req, res, next) => {
+    try {
+        const users = await models.User.find();
+        res.json(users);
+    } catch (err) {
+        console.log("Error fetching users.\n", err);
+        res.json({ Message: "Error fetching users" });
+    }
+});
 
 export default router;
