9b1067c8f9781d8e2e094d258d052ea0b4610de5
chetu97 <chaitanya.sanoriya@gmail.com>
2021-04-19 20:31:44 -0400

    added update for question

--- routes/question.mjs
+++ routes/question.mjs
@@ -1,61 +1,76 @@
-import express from 'express';
-import models from '../db/db_main.mjs';
+import express from "express";
+import models from "../db/db_main.mjs";
 const router = express.Router();
 
-router.get('/', async(req, res, next) => {
+router.get("/", async (req, res, next) => {
     try {
-        const questions = await models.Question.find({isPublic: true}).exec();
+        const questions = await models.Question.find().exec();
         res.json(questions);
-    }
-    catch(err) {
+    } catch (err) {
         console.log(err);
-        res.status(500).json({Message: "Error while fetching questions"});
+        res.status(500).json({ Message: "Error while fetching questions" });
     }
 });
 
-router.post('/add', async(req, res, next) => {
+router.post("/add", async (req, res, next) => {
     try {
         const question = req.body.question;
-        const q = await models.Question({
-            question: question.question,
-            options: question.options,
-            answer: question.answer,
-            type: question.type,
-            isPublic: question.isPublic
-        }).save();
+        const q = await models
+            .Question({
+                question: question.question,
+                options: question.options,
+                answer: question.answer,
+                type: question.type,
+                isPublic: question.isPublic,
+            })
+            .save();
         res.json(q);
-    }
-    catch(err) {
+    } catch (err) {
         console.log(err);
-        res.status(500).json({Message: "Error while adding question!"});
+        res.status(500).json({ Message: "Error while adding question!" });
     }
 });
 
-router.get('/category/:category', async(req, res, next) => {
+router.post("/update/:id", async (req, res, next) => {
+    try {
+        const question = await models.Question.findByIdAndUpdate(
+            req.params.id,
+            {
+                question: req.body.question,
+                options: req.body.options,
+                answer: req.body.answer,
+            }
+        );
+        res.json(question);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ Message: "Error while adding question!" });
+    }
+});
+
+router.get("/category/:category", async (req, res, next) => {
     try {
         const category = req.params.category;
-        const questions = await models.Question.find({type: category, isPublic: true}).exec();
+        const questions = await models.Question.find({
+            type: category,
+            isPublic: true,
+        }).exec();
         res.json(questions);
-    }
-    catch(err) {
+    } catch (err) {
         console.log(err);
-        res.status(500).json({Message: "Error while fetching questions"});
+        res.status(500).json({ Message: "Error while fetching questions" });
     }
 });
 
-router.delete('/delete/:id', async(req, res, next) => {
+router.delete("/delete/:id", async (req, res, next) => {
     try {
         const id = req.params.id;
         await models.Question.findByIdAndDelete(id);
-        res.json({Message: "Question deleted"});
-    }
-    catch(err) {
+        res.json({ Message: "Question deleted" });
+    } catch (err) {
         console.log(err);
-        res.status(500).json({Message: "Error while deleting question"});
+        res.status(500).json({ Message: "Error while deleting question" });
     }
 });
 
-
 export default router;
-
-
